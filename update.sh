5000dd9795fb76be3cfc269c4d1c9b20b12d9c87
Jijeesh <jijeesh.ka@gmail.com>
2019-06-18 12:55:26 +0530

    consul helm added

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
+get_latest_release_number() {
+  curl --silent "https://github.com/$1/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#'
+}
 function get_download_url {
 	# wget -q -nv -O- https://api.github.com/repos/$1/$2/releases/latest 2>/dev/null |  jq -r '.assets[] | select(.browser_download_url |  .browser_download_url'
   wget -q -nv -O- https://api.github.com/repos/$1/$2/releases/latest 2>/dev/null |  jq -r '.assets[] | select(.browser_download_url) | .browser_download_url'
 }
 
 function update_repository {
+
 	URL=$(get_download_url $1 $2)
 
 	BASE=$(basename $URL)
@@ -14,5 +18,5 @@ function update_repository {
 	fi
 
 }
-
-update_repository hashicorp consul-helm
+get_latest_release_number
+# update_repository hashicorp consul-helm
